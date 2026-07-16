6a3e8043c536483a174e192ead12a33cc41d0dfb
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-17 00:58:16 +0530

    readonly code example

---
+++ Program/readonlyConst/readonly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program;
+
+class @readonly
+{
+    readonly int x = 5;
+    static void Main (string[] args)
+    {
+        Program obj = new Program ();
+        Console.WriteLine(obj.x);
+        Console.Read();
+    }
+}

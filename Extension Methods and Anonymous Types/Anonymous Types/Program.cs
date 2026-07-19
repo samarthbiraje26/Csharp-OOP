0fef33033aab1ae32a4d1d0667f1389f7cad1ece
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-20 01:52:48 +0530

    Anonymous code and theory

--- Extension Methods and Anonymous Types/Anonymous Types/Program.cs
+++ Extension Methods and Anonymous Types/Anonymous Types/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+An Anonymous Type lets you create an object — without defining 
+a class — quickly, for temporary use.
+✓ No class required
+✓ Declared with var
+✓ Properties are read-only
+
+
+Syntax :
+var var_name new 
+{
+    Name = "ABC",
+    Dept = "IT"
+};
+Console.WriteLine(var_name.Name);
+
+
+Why var ?
+var lets the compiler infer the generated type for you.
+
+
+Rules of anonymous type - 
+1. No class needed.
+2. Declared with var.
+3. It has read-only properties. Value cannot be changed if assigned once.
+4. Used for temporary and short lived use.
+
+
+Why do we need anonymous type?
+1. It builds only the fields we need.
+2. Saves memory. 
+*/
+
+// Create an anonymous object for a Mobile Phone with these companies, and print all three:
+// Company   ·   Model   ·   Price
+
+var Mobile = new
+{
+    Company = "Samsung",
+    Model = "S24",
+    Price = 50000
+};
+Console.WriteLine(Mobile.Company);
+Console.WriteLine(Mobile.Model);
+Console.WriteLine(Mobile.Price);

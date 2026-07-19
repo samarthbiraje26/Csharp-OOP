8ce82525db1fce0ab3639431e4db1198eacc308c
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-20 02:35:17 +0530

    Backing field code

--- Encapsulation/AutoImplemented and Backing Field/Program.cs
+++ Encapsulation/AutoImplemented and Backing Field/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Encapsulation is the process of hiding the data and controlling access to it using properties or methods.
+
+
+// Backing Field
+
+
+//Create an employee class with -
+//1.A private field_salary 2.A Property Salary 3.Validation : Salary should be greater than 0
+
+using System;
+
+class Employee
+{
+    private int _salary;
+    public int Salary
+    {
+        get { return _salary; }
+
+        set
+        {
+            if (value > 0)
+            {
+                _salary = value;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main ()
+    {
+        Employee emp = new Employee();
+
+        emp.Salary = 50000;
+        Console.WriteLine("Salary: " + emp.Salary);
+    }
+}

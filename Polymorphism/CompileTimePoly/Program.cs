e185a4c3302327f1d7a900e6d8882a4d3e562ad0
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-21 00:09:23 +0530

    Method overloading overview, code examples and practice

--- Polymorphism/CompileTimePoly/Program.cs
+++ Polymorphism/CompileTimePoly/Program.cs
@@ -1,1 +1,78 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Compile Time Polymorphism / Method Overloading - 
+// Same Name , Different Parameters
+
+using System;
+using System.ComponentModel;
+
+class Calculator
+{
+    public void Add (int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+
+    public void Add (int a, int b, int c)
+    {
+        Console.WriteLine(a + b + c);
+    }
+}
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Calculator cal = new Calculator();
+
+        cal.Add(10, 50);
+        cal.Add(5, 40, 50);
+    }
+}
+
+
+/*
+How does C# know which Add() to call?
+
+When you write - cal.Add(10,20);
+
+Compiler checks - 
+Add(int,int)
+
+Exists?
+Yes
+
+↓
+
+Call it.
+
+When you write - cal.Add(10,20,30);
+
+Compiler finds -
+Add(int,int,int)
+
+Call it.
+
+Decision made before the program runs.
+
+Hence, Compile Time Polymorphism.
+*/
+
+
+
+/* 
+Invalid Overloading - 
+
+class Demo
+{
+    public int Show()
+    {
+        return 10;
+    }
+
+    public string Show()
+    {
+        return "Hello";
+    }
+}
+
+Because - Only return type changed. Parameters are the same.
+*/
---
+++ Polymorphism/CompileTimePoly/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Printer
+{
+    public void Print (string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    public void Print (int a)
+    {
+        Console.WriteLine(a);
+    }
+}
+
+class Prog
+{
+    static void Main(string[] args)
+    {
+        Printer p = new Printer ();
+
+        p.Print("I am the best.");
+        p.Print(7);
+    }
+}

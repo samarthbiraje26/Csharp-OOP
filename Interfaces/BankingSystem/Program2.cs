8eb68aa288e2ab5da4f30f70d153dca067987403
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-23 00:25:29 +0530

    interface code practice and example

---
+++ Interfaces/BankingSystem/Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void Speak ()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak ()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Prog
+{
+    static void Main (string[] args)
+    {
+        Animal obj = new Dog();
+
+        obj.Speak ();
+    }
+}

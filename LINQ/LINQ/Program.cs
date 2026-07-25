87e3ea65f7e92cf433d23ceb8d4d6e9ea82466ff
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-25 23:50:01 +0530

    LINQ Code examples

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Question 1 - Multiple Conditions + Projection
+
+Create an Employee class with:
+Id
+Name
+Department
+Age
+Salary
+
+Retrieve the Name and Salary of employees who:
+belong to the IT department
+are older than 25
+earn more than ₹60,000
+
+Sort them by salary in descending order.
+*/
+
+using System;
+using System.Linq;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Department { get; set;}
+    public int Age { get; set; }
+    public int Salary { get; set; }
+
+}
+class Program
+{
+    static void Main ()
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee { Id = 1, Name = "Rahul", Department = "IT", Age = 28, Salary = 70000 },
+            new Employee { Id = 2, Name = "Amit", Department = "HR", Age = 30, Salary = 65000 },
+            new Employee { Id = 3, Name = "Neha", Department = "IT", Age = 24, Salary = 80000 },
+            new Employee { Id = 4, Name = "Priya", Department = "IT", Age = 29, Salary = 55000 },
+            new Employee { Id = 5, Name = "Karan", Department = "IT", Age = 32, Salary = 90000 },
+            new Employee { Id = 6, Name = "Sneha", Department = "Finance", Age = 27, Salary = 85000 }
+
+
+        };
+
+        var result = employees.Where(e => e.Department == "IT" && e.Age > 25 && e.Salary > 60000)
+        .OrderByDescending(e => e.Salary).Select(e => new {e.Name,e.Salary});
+
+        foreach (var emp in result)
+        {
+            Console.WriteLine($"Name : {emp.Name}");
+            Console.WriteLine($"Salary : {emp.Salary}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+
+}
---
+++ LINQ/LINQ/Program2.cs
@@ -0,0 +1,70 @@
+﻿/*
+Create an Employee1 class and a list of employees.
+
+Using LINQ, find:
+
+1. Are there any employees whose salary is greater than 100000?
+2. Are all employees older than 21?
+3. Count employees from the HR department whose salary is greater than 50000.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQPractice
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Department { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee { Id = 1, Name = "Rahul", Department = "IT", Age = 28, Salary = 70000 },
+                new Employee { Id = 2, Name = "Amit", Department = "HR", Age = 30, Salary = 65000 },
+                new Employee { Id = 3, Name = "Neha", Department = "IT", Age = 24, Salary = 80000 },
+                new Employee { Id = 4, Name = "Priya", Department = "HR", Age = 26, Salary = 45000 },
+                new Employee { Id = 5, Name = "Karan", Department = "IT", Age = 32, Salary = 90000 },
+                new Employee { Id = 6, Name = "Sneha", Department = "Finance", Age = 27, Salary = 85000 }
+            };
+
+            // Question 1
+            bool hasHighSalary = employees.Any(e => e.Salary > 100000);
+
+            // Question 2
+            bool allAbove21 = employees.All(e => e.Age > 21);
+
+            // Question 3
+            int hrCount = employees.Count(e => e.Department == "HR" &&
+                                               e.Salary > 50000);
+
+            Console.WriteLine("1. Any employee with salary > 100000 ?");
+            Console.WriteLine(hasHighSalary);
+
+            Console.WriteLine();
+
+            Console.WriteLine("2. Are all employees older than 21 ?");
+            Console.WriteLine(allAbove21);
+
+            Console.WriteLine();
+
+            Console.WriteLine("3. HR employees having salary > 50000");
+            Console.WriteLine(hrCount);
+
+            Console.ReadLine();
+        }
+    }
+}

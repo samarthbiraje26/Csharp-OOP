4a9895a82dbd444c4b6b3836396e6c631529db61
samarthbiraje26 <samarthbiraje@gmail.com>
2026-07-26 20:58:05 +0530

    Code examples for base keyword

--- Encapsulation/Base Keyword/Program.cs
+++ Encapsulation/Base Keyword/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿/*
+Problem Statement
+
+Design a College Management System.
+
+Requirements:
+
+1. Create a class College.
+2. Create a constructor in College that displays: Welcome to ABC College.
+3. Create a child class Student.
+4. Use the base keyword to call the parent constructor.
+5. In Main(), create an object of Student.
+6. Expected Output - Welcome to ABC College
+
+Student object created.
+Concept Tested: base() constructor call
+*/
+
+class College
+{
+    public College ()
+    {
+        Console.WriteLine("Welcome to ABC College");
+    }
+}
+
+class Student : College
+{
+    public Student () : base()
+    {
+        Console.WriteLine("Student object created");
+    }
+}
+
+class Program
+{
+    static void Main ()
+    {
+        Student student = new Student();
+
+        Console.ReadLine ();
+    }
+}
---
+++ Encapsulation/Base Keyword/Program2.cs
@@ -0,0 +1,42 @@
+﻿/*
+Problem Statement
+Design a Hospital Management System.
+
+Requirements:
+1. Create a class Hospital.
+2. Add a method:DisplayHospitalInfo()
+3. Print : City Care Hospital
+4. Create a child class GeneralWard.
+5. Add a method: AdmitPatient()
+6. Inside AdmitPatient(), first call the parent method using base, then print - Patient admitted successfully.
+*/
+
+using System;
+
+class Hospital
+{
+    public void DisplayHospitalInfo ()
+    {
+        Console.WriteLine("City Care Hospital.");
+    }
+}
+
+class GeneralWrad : Hospital
+{
+    public void AdmitPatient ()
+    {
+        base.DisplayHospitalInfo ();
+
+        Console.WriteLine("Patient Admitted successfully");
+    }
+}
+
+class Prog
+{
+    static void Main ()
+    {
+        Hospital hospital = new Hospital();
+
+        Console.ReadLine ();
+    }
+}
